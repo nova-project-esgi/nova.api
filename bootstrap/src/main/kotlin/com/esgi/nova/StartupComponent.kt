61454e781a6ff0dbf239f4361ff7c92d24c23206
BAILLEUL <bailleul.sacha@gmail.com>
2021-01-03 19:38:04 +0100

    [feat] Add fs initialization

--- bootstrap/src/main/kotlin/com/esgi/nova/StartupComponent.kt
+++ bootstrap/src/main/kotlin/com/esgi/nova/StartupComponent.kt
@@ -1,5 +1,7 @@
 package com.esgi.nova
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
@@ -10,12 +12,17 @@ import java.io.File
 class StartupComponent() : CommandLineRunner {
     @Value("\${app.upload.dir:\${user.home}}")
     lateinit var uploadDir: String
-
+    var logger: Logger = LoggerFactory.getLogger(StartupComponent::class.java)
     override fun run(vararg args: String) {
         val dir = File(uploadDir)
         dir.mkdir()
-        File(dir, "resources").mkdir()
-        File(dir, "events").mkdir()
+        logger.info("CREATE UPLOAD DIR : ${dir.absolutePath}")
+        val resourcesDir = File(dir, "resources")
+        logger.info("CREATE RESOURCES DIR : ${resourcesDir.absolutePath}")
+        resourcesDir.mkdir()
+        val eventsDir = File(dir, "events")
+        logger.info("CREATE EVENTS DIR : ${eventsDir.absolutePath}")
+        eventsDir.mkdir()
     }
 
 }
