2796a4d21549931ee926e832448eace04e7bd9d3
BAILLEUL <bailleul.sacha@gmail.com>
2021-01-07 23:04:35 +0100

    [feat] : Refactor imports

--- file-manager/src/main/kotlin/com/esgi/nova/file_manager/configuration/FileWatcherConfig.kt
+++ file-manager/src/main/kotlin/com/esgi/nova/file_manager/configuration/FileWatcherConfig.kt
@@ -1,8 +1,6 @@
 package com.esgi.nova.file_manager.configuration
 
 import com.esgi.nova.file_manager.MyFileChangeListener
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 
 import org.springframework.boot.devtools.filewatch.FileSystemWatcher
@@ -14,20 +12,20 @@ import java.time.Duration
 @Configuration
 open class FileWatcherConfig {
 
-    @Value("\${app.upload.dir:\${user.home}}")
-    lateinit var uploadDir: String
-
-    @Bean
-    open fun fileSystemWatcher(): FileSystemWatcher {
-        val fileSystemWatcher = FileSystemWatcher(true, Duration.ofMillis(100), Duration.ofMillis(100))
-        fileSystemWatcher.addSourceDirectory(File("$uploadDir/resources"))
-        fileSystemWatcher.addSourceDirectory(File("$uploadDir/events"))
-        fileSystemWatcher.addListener(MyFileChangeListener())
-        fileSystemWatcher.start()
-        return fileSystemWatcher
-    }
-
-    fun onDestroy() {
-        fileSystemWatcher().stop()
-    }
+//    @Value("\${app.upload.dir:\${user.home}}")
+//    lateinit var uploadDir: String
+//
+//    @Bean
+//    open fun fileSystemWatcher(): FileSystemWatcher {
+//        val fileSystemWatcher = FileSystemWatcher(true, Duration.ofMillis(100), Duration.ofMillis(100))
+//        fileSystemWatcher.addSourceDirectory(File("$uploadDir/resources"))
+//        fileSystemWatcher.addSourceDirectory(File("$uploadDir/events"))
+//        fileSystemWatcher.addListener(MyFileChangeListener())
+//        fileSystemWatcher.start()
+//        return fileSystemWatcher
+//    }
+//
+//    fun onDestroy() {
+//        fileSystemWatcher().stop()
+//    }
 }
