91f1d23840e4e38fcc9d0acf8989542b5db366d2
Stanislas Lange <stanislas.lange@pm.me>
2021-02-02 23:02:52 +0100

    chore(build): update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,10 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.4.10" apply false
-    kotlin("plugin.spring") version "1.4.10" apply false
-    id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
-    id("org.springframework.boot") version "2.4.0" apply false
+    kotlin("jvm") version "1.4.21" apply false
+    kotlin("plugin.spring") version "1.4.21" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    id("org.springframework.boot") version "2.4.2" apply false
 }
 
 extra.apply {
@@ -66,6 +66,3 @@ subprojects {
         }
     }
 }
-
-
-
