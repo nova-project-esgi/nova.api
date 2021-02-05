9fb12c1beffc1f6d42086932c155004bd922907f
BAILLEUL <bailleul.sacha@gmail.com>
2021-02-05 14:01:11 +0100

    [fix]: Change event storage type

--- bootstrap/src/main/kotlin/com/esgi/nova/config/AxonConfiguration.kt
+++ bootstrap/src/main/kotlin/com/esgi/nova/config/AxonConfiguration.kt
@@ -1,18 +1,29 @@
 package com.esgi.nova.config
 
+import org.axonframework.common.jdbc.ConnectionProvider
+import org.axonframework.common.transaction.NoTransactionManager
 import org.axonframework.config.EventProcessingConfigurer
-import org.axonframework.eventhandling.*
-import org.axonframework.eventhandling.ListenerInvocationErrorHandler
+import org.axonframework.eventsourcing.eventstore.jdbc.JdbcEventStorageEngine
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.annotation.PostConstruct
 
 
 @Configuration
-class AxonConfig @Autowired constructor(private val configurer: EventProcessingConfigurer) {
+open class AxonConfig @Autowired constructor(private val configurer: EventProcessingConfigurer) {
 
     @PostConstruct
     fun registerErrorHandling() {
         configurer.usingSubscribingEventProcessors()
     }
+
+    @Bean
+    open fun eventStorageEngine(connectionProvider: ConnectionProvider): JdbcEventStorageEngine? {
+        return JdbcEventStorageEngine.builder()
+                .connectionProvider(connectionProvider)
+                .transactionManager(NoTransactionManager.INSTANCE)
+                .build()
+    }
 }
+
